b8993c55125ffe83d44bafac1cc39fb4714c504d
Vdot Babyy <130109175+VdotBabyy@users.noreply.github.com>
2023-10-10 08:28:16 -0400

    Update Priv.lua

--- Priv.lua
+++ Priv.lua
@@ -1,2 +1,2 @@
-script_key = getgenv().Kos.Options.Key;
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a959233d83b5d880cdf1465025c519b5.lua"))()
+script_key = getgenv().Priv.Options.Key;
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c981248308ad477c4336412811ec19b8.lua"))()
